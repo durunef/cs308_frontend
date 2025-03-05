c183824cc70c13198bba9f1c9495600b2ae88e16
Duru Nef <durunefozmen@Duru-MacBook-Pro.local>
2025-03-05 13:01:35 +0300

    Initial commit - React setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="name-container">
+      <h1>Duru</h1>
+      <h1>Serhat</h1>
+      <h1>Ömer</h1>
+      <h1>Arda</h1>
+      <h1>Deren</h1>
+    </div>
+  )
+}
+
+export default App
